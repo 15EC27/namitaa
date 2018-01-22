2d0455c270fc9e0b3c89a4461fa3a47f0c045747
15EC27 <namimadhan@gmail.com>
2018-01-22 10:58:05 +0530

    Update vc.c

--- vc.c
+++ vc.c
@@ -3,6 +3,7 @@ int main()
 {
 char c;
 int uvowel,lvowel;
+printf("Enter the character:");  
 scanf("%d",&c);
 uvowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 lowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
@@ -11,3 +12,4 @@ if(uvowel || lvowel)
 else
    printf("%d" is a consonant",c);
 return 0;
+}         
