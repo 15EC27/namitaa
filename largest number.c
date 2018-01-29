cb524f73be2b30476704500da6ed9ab0427a6ff5
15EC27 <namimadhan@gmail.com>
2018-01-29 09:51:37 +0530

    Create largest number.c

---
+++ largest number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,temp;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b)
+  temp=a;
+else temp=b;
+if(temp>c)
+  l=temp;
+else l=c;
+printf("%d",l);
+return 0;
+}
