c6529a0d89719e21bb4567b2ae52a491f655b30e
15EC27 <namimadhan@gmail.com>
2018-02-12 10:03:49 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,7 +3,7 @@ int main()
 {
 int n,i,count=0;
 scanf("%d",&n);
-for(i=2;i<n,i++)
+for(i=2;i<n;i++)
 {
    if((n%i)==0)
    {
@@ -12,7 +12,7 @@ for(i=2;i<n,i++)
    }
 }
 if(count==0)
-  printf("yes")
+  printf("yes");
 else
   printf("no");
 return 0;
